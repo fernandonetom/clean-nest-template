09d3d94ea37eb47c3a249f036d2e6795a1eed428
Fernando Neto <fernando39357@gmail.com>
2023-05-06 18:46:37 -0300

    test: update jest config

--- jest.config.js
+++ jest.config.js
@@ -1,11 +1,10 @@
 module.exports = {
   moduleFileExtensions: ['js', 'json', 'ts'],
-  rootDir: 'test',
   testRegex: '.*\\.(spec|test)\\.ts$',
   transform: {
     '^.+\\.(t|j)s$': 'ts-jest',
   },
-  collectCoverageFrom: ['**/*.(t|j)s'],
-  coverageDirectory: '../coverage',
+  collectCoverageFrom: ['src/**/*.(t|j)s'],
+  coverageDirectory: './coverage',
   testEnvironment: 'node',
 };
